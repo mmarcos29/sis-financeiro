dc8f42165511175325a25d66391884df506f4bc0
josianegomes21 <josiane.gomes2106@gmail.com>
2020-08-11 17:56:33 -0300

    adicionando componente BarraLocation

---
+++ src/componentes/navs-e-conteudo/Conteudo/PesquisaInss/BarraLocation/BarraLocation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Styles from './Styles.css'
+
+class BarraLocation extends Component {
+    render() {
+        return (
+            <div id="BarraLocation">
+                <div>ADONIAS INACIO DA SILVA</div>                
+            </div>
+        );
+    }
+}
+
+export default BarraLocation;
