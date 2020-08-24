249758a3a07c916ae8c23c4a767c11a293ab0f4c
josianegomes21 <josiane.gomes2106@gmail.com>
2020-08-24 17:04:33 -0300

    Adicionamamos as funcinalidades do cartão cliente

--- src/componentes/navs-e-conteudo/Conteudo/SimulacaoProposta/FuncionalidadesClientes/FuncionalidadesClientes.js
+++ src/componentes/navs-e-conteudo/Conteudo/SimulacaoProposta/FuncionalidadesClientes/FuncionalidadesClientes.js
@@ -6,27 +6,36 @@ class FuncionalidadesClientes
     render() {
         return (
             <div id="FuncionalidadesClientes">
-                <div id="Informacoes">
-                    <div className="caixa"><p>Consignável?</p>  <p id="sim">SIM</p></div>
-                    <div className="caixa"> <p>Espécie?</p>  <p>21</p> </div>
-                    <div className="caixa"> <p>Situação</p>  <p>ATIVO</p> </div>
-                    <div className="caixa"> <p>Competência</p>  <p>05/2020</p> </div>
-                    <div className="caixa"> <p>DIB</p>  <p>13/07//2013</p> </div>
-                    <div className="caixa"> <p>DDB</p>  <p>10/08/2013</p> </div>
-                    <div className="caixa"><p>Base MR</p>  <p>R$1.045,00 (25%)</p> </div>
-                    <div className="caixa"><p>Valor MR</p>  <p>R$1.045,00</p> </div>
-                    <div id="caixaMargem1"><p>MARGEM PARA EMPRESTIMO</p> <p>R$ 313,50 (25%)</p> </div>
-                    <div id="liberado1"><p>LIBERADO</p> <p>R$ 12.928,36</p> </div>
-                    <div id="caixaMargem2"><p>MARGEM PARA CARTÃO RMC</p> <p>R$ 52,25 (5%)</p> </div>
-                    <div id="liberado2"><p>LIBERADO</p> <p>R$ 1.389,25</p> </div>
+                <div id="Funcionalidades">
+                    <div id="Beneficiario">
+                        <p> ADONIAS INACIO DA SILVA</p>
+                        <p> 89 ANOS </p>
+                    </div>
+                    <div id="BordaSuperior"></div>
+                    <div className="caixa"><p>Consignável?</p> <p id="SIM">SIM</p></div>
+                    <div className="caixa"><p>Espécie?</p> <p className="Respostas">21</p> </div>
+                    <div className="caixa"><p>Situação</p> <p className="Respostas">ATIVO</p> </div>
+                    <div className="caixa"><p>Competência</p> <p className="Respostas">05/2020</p> </div>
+                    <div className="caixa"><p>DIB</p> <p className="Respostas">13/07//2013</p> </div>
+                    <div className="caixa"><p>DDB</p> <p className="Respostas">10/08/2013</p> </div>
+                    <div className="caixa"><p>Base MR</p> <p className="Respostas">R$1.045,00 (25%)</p> </div>
+                    <div className="caixa"><p>Valor MR</p> <p className="Respostas">R$1.045,00</p> </div>
+
+                    <div id="MargemEmprestimo">
+                        <div id="Margem1"><p>MARGEM PARA EMPRESTIMO</p> <p className="ValorLiberado">R$ 313,50 (25%)</p> </div>
+                        <div id="Liberado1"><p className="Liberado">LIBERADO</p> <p>R$ 12.928,36</p> </div>
+                    </div>
+                    <div id="MargemCartão">
+                        <div id="Margem2"><p>MARGEM PARA CARTÃO RMC</p> <p className="ValorLiberado">R$ 52,25 (5%)</p> </div>
+                        <div id="Liberado2"><p className="Liberado">LIBERADO</p> <p>R$ 1.389,25</p> </div>
+                    </div>
                 </div>
-                <div>
-
-                </div>
-
-
 
             </div>
+
+
+
+            </div >
         );
     }
 }
