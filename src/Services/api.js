d561ff67587a6ff9064cc9a999b0b68cf10f4ffb
mmarcos29 <joaomarcos1916@gmail.com>
2020-09-27 11:30:13 -0300

    adicionando tabela listagem de clientes

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: 'http://3.22.178.76:9000/api/receivefile',
-    baseURL: 'https://localhost:5001/api/receivefile',
+    baseURL: 'http://3.22.178.76:9000/api/receivefile',
+    // baseURL: 'https://localhost:5001/api/receivefile',
     timeout: 9999999
     
   });
--- src/componentes/navs-e-conteudo/Conteudo/Clientes/Clientes.jsx
+++ src/componentes/navs-e-conteudo/Conteudo/Clientes/Clientes.jsx
@@ -32,6 +32,40 @@ export default class Clientes extends Component {
         <BarraLocationPage incluir={<IconeIncluir />}>
           {[...this.props.children]}
         </BarraLocationPage>
+
+        {/* <h2><b>Easy Clinic - alunos</b></h2> */}
+                    {/* <NavFuncionalidade rota="aluno-cadastro" />                     */}
+                    <table id="alunos">
+                        <thead>
+                            <tr>
+                                <th className="espacamento"><b>ID Cliente</b></th>
+                                <th className="espacamento"><b>NOME</b></th>
+                                <th className="espacamento"><b>CPF</b></th>
+                                <th className="espacamento"><b>-----</b></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>1</td>
+                            <td>João marcos dos santos batista</td>
+                            <td>07360473365</td>
+                            <td>-----</td>
+                          </tr>
+                            {/* {this.state.Alunos.map(aluno =>
+                                <tr id={aluno.index}>
+                                    <td>{aluno.id}</td>
+                                    <td>{aluno.nome}</td>
+                                    <td>{aluno.clienteId}</td>
+                                    <td id="acoes">
+                                        <button className="btnEditar" onClick={this.atualizar}>Editar</button>
+                                        <button className="btnCancelar" onClick={() => this.deletar(aluno.id)}>Excluir</button>
+                                    </td>
+                                </tr>    
+                            )}
+                            {console.log(this.state.Alunos)} */}
+                            
+                        </tbody>                        
+                    </table>
       </div>
     );
   }
