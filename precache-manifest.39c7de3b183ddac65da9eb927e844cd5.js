1432bd280be5cefc20fa8d415405290d64494547
mmarcos29 <joaomarcos1916@gmail.com>
2021-01-23 13:39:38 -0300

    Updates

---
+++ precache-manifest.39c7de3b183ddac65da9eb927e844cd5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c71451defa8cb6b9a896e2f53abc4d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e4add54f66fed83134",
+    "url": "/static/css/main.9262ccd1.chunk.css"
+  },
+  {
+    "revision": "1c540bd712a25e3fb86b",
+    "url": "/static/js/2.450f5cda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.450f5cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e4add54f66fed83134",
+    "url": "/static/js/main.e390ecd1.chunk.js"
+  },
+  {
+    "revision": "8bcedf9ca0e068e3486a",
+    "url": "/static/js/runtime-main.7f01eb7f.js"
+  },
+  {
+    "revision": "4523e44fd89e148a28d5e124d9329868",
+    "url": "/static/media/agendar cliente.4523e44f.png"
+  },
+  {
+    "revision": "26877566b1c9cc00edfb09c614f8097a",
+    "url": "/static/media/amarelo.26877566.png"
+  },
+  {
+    "revision": "c8c4fc47222113bfdc391a33b923b5b3",
+    "url": "/static/media/atender cliente.c8c4fc47.png"
+  },
+  {
+    "revision": "6b56fecbb54b41ab9e1b546645e35615",
+    "url": "/static/media/azul.6b56fecb.png"
+  },
+  {
+    "revision": "a08ac5c14b38fa684c872748814c0d1a",
+    "url": "/static/media/cadastrar clientes.a08ac5c1.png"
+  },
+  {
+    "revision": "7b03dfac5459ba45add9ddf8e3613904",
+    "url": "/static/media/efetivar proposta.7b03dfac.png"
+  },
+  {
+    "revision": "1cc4f3c7194351c7bea2e16a085a20ac",
+    "url": "/static/media/loader.1cc4f3c7.gif"
+  },
+  {
+    "revision": "6690acdb5abec2512b31ed5f2748c90f",
+    "url": "/static/media/logo-revolution.6690acdb.png"
+  },
+  {
+    "revision": "7929a49aa3b71666795e7ea992c470c5",
+    "url": "/static/media/roxo.7929a49a.png"
+  },
+  {
+    "revision": "56c45fcc0f606fe7aef2cb8481d3996b",
+    "url": "/static/media/verde.56c45fcc.png"
+  },
+  {
+    "revision": "36e609fd2d6d9eebe65ed5884610fbbf",
+    "url": "/static/media/vermelho.36e609fd.png"
+  }
+]);
